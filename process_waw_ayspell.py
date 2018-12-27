05b87a87185b2649c9f813e49c3ce84aefda3cd8
Motaz Saad <motaz.saad@gmail.com>
2018-12-27 17:11:31 +0200

    add rooting and spell checking methods

---
+++ process_waw_ayspell.py
@@ -0,0 +1,36 @@
+import argparse
+import aspell
+
+
+def separate_waw(text):
+    ar_spell = aspell.Speller('lang', 'ar')
+    words = line.split()
+    sentence = ''
+    for word in words:
+        if word.startswith('و'):
+            if word in ar_spell:
+                sentence += word + ' '
+            else:
+                sentence += 'و ' + word[1:] + ' '
+                print('{} changed to {}'.format(word, 'و ' + word[1:]))
+        else:
+            sentence += word + ' '
+    return sentence
+
+
+parser = argparse.ArgumentParser(description='separate the '
+                                             'conjunction waw from '
+                                             'Arabic words')
+
+parser.add_argument('-i', '--infile', type=argparse.FileType(mode='r', encoding='utf-8'),
+                    help='input file.', required=True)
+parser.add_argument('-o', '--outfile', type=argparse.FileType(mode='w', encoding='utf-8'),
+                    help='out file.', required=True)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    lines = args.infile.readlines()
+    clean_lines = list()
+    for line in lines:
+        clean_lines.append(separate_waw(line))
+    args.outfile.write('\n'.join(clean_lines))
---
+++ process_waw_rooting.py
@@ -0,0 +1,43 @@
+import argparse
+
+from nltk.stem.isri import ISRIStemmer
+
+
+def get_root_word(arabic_word):
+    arabic_stemmer = ISRIStemmer()
+    arabic_root = arabic_stemmer.stem(arabic_word)
+    return arabic_root
+
+
+def separate_waw(text):
+    words = line.split()
+    sentence = ''
+    for word in words:
+        if word.startswith('و'):
+            root = get_root_word(word)
+            if root.startswith('و'):
+                sentence += word + ' '
+            else:
+                sentence += 'و ' + word[1:] + ' '
+                print('{} changed to {}'.format(word, 'و ' + word[1:]))
+        else:
+            sentence += word + ' '
+    return sentence
+
+
+parser = argparse.ArgumentParser(description='separate the '
+                                             'conjunction waw from '
+                                             'Arabic words')
+
+parser.add_argument('-i', '--infile', type=argparse.FileType(mode='r', encoding='utf-8'),
+                    help='input file.', required=True)
+parser.add_argument('-o', '--outfile', type=argparse.FileType(mode='w', encoding='utf-8'),
+                    help='out file.', required=True)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    lines = args.infile.readlines()
+    clean_lines = list()
+    for line in lines:
+        clean_lines.append(separate_waw(line))
+    args.outfile.write('\n'.join(clean_lines))
